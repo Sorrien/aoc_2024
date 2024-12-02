14e0631ab2029cf6f652c2b158ead76a97094249
Sorrien <sorrien.sorrowind@gmail.com>
2024-12-02 12:30:53 -0500

    day 2 completed

---
+++ src/day2/part2.rs
@@ -0,0 +1,59 @@
+pub fn solution(input: String) -> u32 {
+    let safe_count = input
+        .lines()
+        .map(|line| {
+            line.split_ascii_whitespace()
+                .map(|x| x.parse::<u32>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .filter(|report| {
+            let mut is_safe = is_report_safe(report);
+
+            if !is_safe {
+                is_safe = attempt_problem_dampener(report);
+            }
+
+            is_safe
+        })
+        .count();
+    safe_count as u32
+}
+
+fn is_report_safe(report: &Vec<u32>) -> bool {
+    let mut is_safe = false;
+    let is_increasing = (report[0] as i32 - report[1] as i32) > 0;
+    for i in 0..report.len() - 1 {
+        let a = report[i];
+        let b = report[i + 1];
+        let abs_diff = a.abs_diff(b);
+        if abs_diff > 0 && abs_diff < 4 {
+            let diff = a as i32 - b as i32;
+
+            if (is_increasing && diff > 0) || (!is_increasing && diff < 0) {
+                is_safe = true;
+            } else {
+                is_safe = false;
+                break;
+            }
+        } else {
+            is_safe = false;
+            break;
+        }
+    }
+
+    is_safe
+}
+
+fn attempt_problem_dampener(report: &Vec<u32>) -> bool {
+    let mut is_safe = false;
+    for i in 0..report.len() {
+        let mut report_clone = report.clone();
+        report_clone.remove(i);
+
+        is_safe = is_report_safe(&report_clone);
+        if is_safe {
+            break;
+        }
+    }
+    is_safe
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 pub mod day1;
+pub mod day2;
 
 #[cfg(test)]
 mod tests {
     use std::fs;
 
     use crate::day1;
+    use crate::day2;
 
     #[test]
     fn day1_part1() {
@@ -22,5 +24,41 @@ mod tests {
 
         let result = day1::part2::solution(input_string);
         assert_eq!(result, 19437052);
+    }
+
+    #[test]
+    fn day2_part1_ex() {
+        let input_string =
+            fs::read_to_string("inputs/day2_example.txt").expect("failed to load input file");
+
+        let result = day2::part1::solution(input_string);
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn day2_part1() {
+        let input_string =
+            fs::read_to_string("inputs/day2.txt").expect("failed to load input file");
+
+        let result = day2::part1::solution(input_string);
+        assert_eq!(result, 502);
+    }
+
+    #[test]
+    fn day2_part2_ex() {
+        let input_string =
+            fs::read_to_string("inputs/day2_example.txt").expect("failed to load input file");
+
+        let result = day2::part2::solution(input_string);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn day2_part2() {
+        let input_string =
+            fs::read_to_string("inputs/day2.txt").expect("failed to load input file");
+
+        let result = day2::part2::solution(input_string);
+        assert_eq!(result, 544);
     }
 }
