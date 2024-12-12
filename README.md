9b91ace893f13f73003fe0902650ff8519e11e7a
Sorrien <sorrien.sorrowind@gmail.com>
2024-12-12 13:46:34 -0500

    replace examples to run with a console app with args to run

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-My project for the 2024 Advent of Code. Every day and part is runnable via --examples and tests.
+My project for the 2024 Advent of Code. Every day and part can be passed in as commandline arguments or run as a test:
+examples:
+cargo run 1 2 (runs day 1 part 2)
+cargo run 10 2 true 1000 (runs day 10 part 2 and measure it by running it 1000 times and averaging the timings.)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,108 @@
+use std::{env, fs, time::Instant};
+
+use aoc_2024::{day1, day10, day11, day2, day3, day4, day5, day6, day7, day8, day9};
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<_>>();
+
+    let day = &args[1];
+    let part = &args[2];
+
+    let day = day.parse::<u8>().unwrap();
+    let part = part.parse::<u8>().unwrap();
+
+    let file_name = format!("inputs/day{}.txt", day);
+    let input = fs::read_to_string(file_name).expect("failed to load input file");
+
+    let measure = if let Some(measure) = args.get(3) {
+        measure.parse::<bool>().unwrap()
+    } else {
+        false
+    };
+
+    let measure_count = if let Some(count) = args.get(4) {
+        count.parse::<u64>().unwrap()
+    } else {
+        100
+    } as usize;
+
+    let result = run_solution(&input, day, part);
+
+    if measure {
+        let mut timings: Vec<u128> = vec![];
+        for _ in 0..measure_count {
+            let start = Instant::now();
+            run_solution(&input, day, part);
+            timings.push((Instant::now() - start).as_nanos());
+        }
+        println!(
+            "avg time in nanos: {}",
+            timings.iter().sum::<u128>() as f64 / timings.iter().count() as f64
+        );
+    }
+
+    println!("{}", result);
+}
+
+pub fn run_solution(input: &String, day: u8, part: u8) -> String {
+    let input = input.clone();
+    let result = match day {
+        1 => match part {
+            1 => day1::part1::solution(input).to_string(),
+            2 => day1::part2::solution(input).to_string(),
+            _ => panic!("Part not recognized"),
+        },
+        2 => match part {
+            1 => day2::part1::solution(input).to_string(),
+            2 => day2::part2::solution(input).to_string(),
+            _ => panic!("Part not recognized"),
+        },
+        3 => match part {
+            1 => day3::part1::solution(input).to_string(),
+            2 => day3::part2::solution(input).to_string(),
+            _ => panic!("Part not recognized"),
+        },
+        4 => match part {
+            1 => day4::part1::solution(input).to_string(),
+            2 => day4::part2::solution(input).to_string(),
+            _ => panic!("Part not recognized"),
+        },
+        5 => match part {
+            1 => day5::part1::solution(input).to_string(),
+            2 => day5::part2::solution(input).to_string(),
+            _ => panic!("Part not recognized"),
+        },
+        6 => match part {
+            1 => day6::part1::solution(input).to_string(),
+            2 => day6::part2::solution(input).to_string(),
+            _ => panic!("Part not recognized"),
+        },
+        7 => match part {
+            1 => day7::part1::solution(input).to_string(),
+            2 => day7::part2::solution(input).to_string(),
+            _ => panic!("Part not recognized"),
+        },
+        8 => match part {
+            1 => day8::part1::solution(input).to_string(),
+            2 => day8::part2::solution(input).to_string(),
+            _ => panic!("Part not recognized"),
+        },
+        9 => match part {
+            1 => day9::part1::solution(input).to_string(),
+            2 => day9::part2::solution(input).to_string(),
+            _ => panic!("Part not recognized"),
+        },
+        10 => match part {
+            1 => day10::part1::solution(input).to_string(),
+            2 => day10::part2::solution(input).to_string(),
+            _ => panic!("Part not recognized"),
+        },
+        11 => match part {
+            1 => day11::part1::solution(input).to_string(),
+            2 => day11::part2::solution(input).to_string(),
+            _ => panic!("Part not recognized"),
+        },
+        _ => panic!("Haven't done that yet I guess"),
+    };
+    result
 }
